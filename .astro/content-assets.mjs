cc74a1af2b2e3b74800ab7788a79095e287ec1ae
donglei <donglei@efreight.cn>
2026-01-08 09:50:17 +0800

    首页图片路径错误

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
+import __ASTRO_IMAGE_IMPORT_qMXUx from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond.md";
 import __ASTRO_IMAGE_IMPORT_18Olpn from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fstart.mdx";
 import __ASTRO_IMAGE_IMPORT_1kauwE from "../../assets/img/2024/01.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftop.md";
-import __ASTRO_IMAGE_IMPORT_qMXUx from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond.md";
-export default new Map([["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fstart.mdx", __ASTRO_IMAGE_IMPORT_18Olpn], ["../../assets/img/2024/01.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftop.md", __ASTRO_IMAGE_IMPORT_1kauwE], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond.md", __ASTRO_IMAGE_IMPORT_qMXUx]]);
+export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond.md", __ASTRO_IMAGE_IMPORT_qMXUx], ["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fstart.mdx", __ASTRO_IMAGE_IMPORT_18Olpn], ["../../assets/img/2024/01.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ftop.md", __ASTRO_IMAGE_IMPORT_1kauwE]]);
 		
